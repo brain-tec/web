f1a78223897203d86efce05e6922f3e602132273
BT-AEsteves <alex.esteves@braintec-group.com>
2020-02-26 16:47:32 +0100

    [MIG] web_widget_timepicker: Migration to 12.0

---
+++ web_widget_timepicker/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2020 brain-tec AG (<http://www.vividlab.de>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Web Timepicker Widget",
+    "version": "12.0.1.0.0",
+    "author": "VividLab, "
+              "Kaushal Prajapati, "
+              "Alexandre Díaz, "
+              "Odoo Community Association (OCA)"
+              "Alex Esteves",
+    "license": "AGPL-3",
+    "category": "Web",
+    "website": "https://github.com/OCA/web/",
+    'installable': True,
+    "depends": [
+        "web",
+    ],
+    "data": [
+        "views/web_widget_timepicker_assets.xml",
+    ],
+}
---
+++ web_widget_timepicker/readme/CONTRIBUTORS.rst
@@ -0,0 +1,4 @@
+* Michael Fried <Michael.Fried@vividlab.de>
+* Kaushal Prajapati <kbprajapati@live.com>
+* Alexandre Díaz <dev@redneboa.es>
+* Alex Esteves <alex.esteves@braintec-group.com>
